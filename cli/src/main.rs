39892c30425985a282373822e0c2e970be86517f
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-12-10 00:59:07 +0100

    WIP cli argument parsing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Write};
 
-use clap::Parser;
+use clap::{CommandFactory, Parser, Subcommand};
 use emulator::Emulator;
 use tui::ui::EmulatorVisualizer;
 
@@ -10,46 +10,55 @@ CLI usage:
 - TODO: `myprogram assemble <file>.asm [-o <output>.fmem]` - assemble
 */
 
-// #[derive(Parser, Debug)]
-// #[command(about)]
-// struct Cli {}
+#[derive(Parser, Debug)]
+#[command(name = "Flisp Emulator", version, about = "Unified tool for the Flisp Emulator", long_about = None)]
+struct Cli {
+    #[arg(global = true)]
+    fmem_file: Option<String>,
 
-fn main() {
-    // let args = Cli::parse();
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(about = "Assemble an assembly file into a .fmem file")]
+    Assemble {
+        #[arg(short, long, default_value = "output.fmem")]
+        output: String,
+        input: String,
+    },
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    dbg!(&args);
+
+    match args.command {
+        Some(Commands::Assemble { input, output }) => {
+            // run_assemble(input, output)?;
+            todo!();
+        }
+        None => {
+            if let Some(fmem) = args.fmem_file {
+                run_visualize(fmem);
+            } else {
+                Cli::command().print_help()?;
+                println!();
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn run_visualize(fmem: String) {
     let mut program = Emulator::default();
-    let mut data = [0_u8; 256];
+    let mut file = File::open(fmem).expect("Failed to open fmem file");
+    todo!();
+    // program
+    //     .load_memory_from_file(&mut file)
+    //     .expect("Failed to load fmem file into memory");
 
-    // Start vector
-    data[0xff] = 0x20;
-
-    // Array
-    data[0x00] = 0x01;
-    data[0x01] = 0x02;
-    data[0x02] = 0x03;
-    data[0x03] = 0x04;
-
-    // Assembly
-    data[0x20] = 0x05; // CLRA
-    data[0x21] = 0x90; // LDX #Data
-    data[0x22] = 0x00; // Data = 0
-    data[0x23] = 0x92; // LDSP #Data
-    data[0x24] = 0x20; // Data = 0x20
-
-    data[0x25] = 0xc6; // ADDA n,X
-    data[0x26] = 0x00; // n = 0
-    data[0x27] = 0xc6; // ADDA n,X
-    data[0x28] = 0x01; // n = 1
-    data[0x29] = 0xc6; // ADDA n,X
-    data[0x2a] = 0x02; // n = 2
-    data[0x2b] = 0xc6; // ADDA n,X
-    data[0x2c] = 0x03; // n = 3
-    data[0x2d] = 0x10; // PSHA
-
-    program.load_memory(&data);
-
-    let mut file = File::create("output.fmem").unwrap();
-    file.write_all(&data).unwrap();
-
-    EmulatorVisualizer::viz(&mut program).unwrap();
+    // EmulatorVisualizer::viz(&mut program).unwrap();
 }
