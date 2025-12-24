82518586ccc1a26e014ce1f70f95f23e71c0f118
Elias Wennerlund <elias06wennerlund@gmail.com>
2025-12-24 01:37:51 +0100

    One more test

--- assembler/tests/tests.rs
+++ assembler/tests/tests.rs
@@ -72,3 +72,31 @@ fn all_suba() {
     FCB $20",
     )
 }
+
+#[test]
+fn read_ascii_from_input() {
+    make_test(
+        "
+DIPSWITCH:      EQU $FC
+SEGMENT7:       EQU $FB
+SEG_ERROR:      EQU %01111001
+                ORG $0
+seg_table:      FCB 63,6,91,79,102,109,125,7,127,111
+                ORG $20
+DisplaySegE:    LDX Segmentkod      ; X <- Segmentkod
+DisplaySegE_1:  LDA DIPSWITCH       ; A <- M(DIPSWITCH)
+                CMPA #10            ; A<10 ?
+                BLO DisplaySegE_2 	; YES
+                LDA #SEG_ERROR		  ; NO
+                JMP DisplaySegE_3
+DisplaySegE_2:  LDA A,X             ; A <- M(A+X)
+DisplaySegE_3:  STA SEGMENT7        ; M(SEGMENT7) <- A
+                JMP DisplaySegE_1
+                ORG SEGMENT7
+Segmentkod:     FCB $0
+                FCB $0
+                ORG $FF
+                FCB $20
+",
+    )
+}
